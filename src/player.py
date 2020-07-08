b916ff0196010e3418fd89f3f270c9d752866541
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-08 13:21:17 -0700

    created player and room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class player: 
+    def __init__(self, id, name):
+        self.id = id 
+        self.name = name
+    
+    def __str__(self):
+        return f"{self.id}: {self.name}"
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class room: 
+    def __init__(self, name, description):
+        self.name = name    
+        self.description = description
+        
